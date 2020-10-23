e9bf604fa6ec232b32d9f11efb445a17fd8140c4
Salvatore Borgia <sa.borgia3@gmail.com>
2020-10-23 13:53:25 +0200

    commenti logici js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// creo array cognomi
+
+// chiedo all'utente il cognome
+
+// pusho il cognome inserito dall'utente nell' array
+
+// stampo la lista ordinata per cognome
+
+//dico all'utente a che posizione si trova della lista
