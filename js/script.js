c7c11a3166f9292f723e3f6fa6cb73d5607ca148
Salvatore Borgia <sa.borgia3@gmail.com>
2020-10-23 18:40:41 +0200

    funzione migliorata con lista disordinata

--- js/script.js
+++ js/script.js
@@ -11,11 +11,44 @@ button.addEventListener("click" ,
     function () {
         // chiedo all'utente il cognome
         var askName = document.getElementById('ask-name');
-
+        
         console.log("Il nome inserito è: ", askName.value);
 
         // pusho il cognome inserito dall'utente nell' array
         listaCognomi.push(askName.value);
+        console.log(listaCognomi);
+
+        //ordino e stampo lista ordinata in log
+        console.log("Lista non ordinata: ", listaCognomi);
+
+        //variabile per loop lista nomi
+        var testostampato = "";
+
+        //ciclo per traversare nomi lista
+        for (var i = 0; i < listaCognomi.length; i++) {
+
+            console.log(listaCognomi[i]);
+
+            testostampato = testostampato + "<li>" + listaCognomi[i] + "</li>";
+        }
+
+        //stampo lista in <li>
+        document.getElementById("lista-2").innerHTML = testostampato;
+
+        console.log(testostampato);
+        
+        // stampo posizione lista
+        var positionList = listaCognomi.indexOf(askName.value);
+        console.log("La tua posizione è la numero: " , positionList + 1);
+
+        document.getElementById('position-2').innerHTML ="posizione numero: " + (positionList + 1); 
+
+        // chiedo all'utente il cognome
+        var askName = document.getElementById('ask-name');
+
+        console.log("Il nome inserito è: ", askName.value);
+
+        // pusho il cognome inserito dall'utente nell' array
         console.log(listaCognomi);
 
         //ordino e stampo lista ordinata in log
@@ -37,13 +70,13 @@ button.addEventListener("click" ,
         document.getElementById("lista").innerHTML = testostampato;
 
         console.log(testostampato);
-        
+
         // stampo posizione lista
         var positionList = listaCognomi.indexOf(askName.value);
-        console.log("La tua posizione è la numero: " , positionList + 1);
+        console.log("La tua posizione è la numero: ", positionList + 1);
 
-        document.getElementById('position').innerHTML ="posizione numero: " + (positionList + 1); 
-
+        document.getElementById('position').innerHTML = "posizione numero: " + (positionList + 1);
     }
+    
 
 )
