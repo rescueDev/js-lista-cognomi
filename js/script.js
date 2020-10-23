9e7b560deb2ab0b03430611f005b66009229d0ab
Salvatore Borgia <sa.borgia3@gmail.com>
2020-10-23 15:44:25 +0200

    script completo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
 // creo array cognomi
+var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 
-// chiedo all'utente il cognome
+console.log(listaCognomi);
 
-// pusho il cognome inserito dall'utente nell' array
+
 
 // stampo la lista ordinata per cognome
+var button = document.getElementById('check-button');
+button.addEventListener("click" , 
 
-//dico all'utente a che posizione si trova della lista
+function () {
+    // chiedo all'utente il cognome
+    var askName = document.getElementById('ask-name');
+
+    console.log("Il nome inserito è: ", askName.value);
+
+    // pusho il cognome inserito dall'utente nell' array
+    listaCognomi.push(askName.value);
+    console.log(listaCognomi);
+
+    //stampo posizione lista ordinata
+    listaCognomi.sort();
+    console.log("Lista ordinata: ", listaCognomi);
+
+    document.getElementById("lista").innerHTML = "La lista dei cognomi è: " + "<br>" + listaCognomi;
+
+    
+    
+    // stampo posizione lista
+    var positionList = listaCognomi.indexOf(askName.value);
+    console.log("La tua posizione è la numero: " , positionList + 1);
+
+    document.getElementById('position').innerHTML ="posizione numero: " + (positionList + 1); 
+
+
+
+
+
+
+
+    }
+
+)
